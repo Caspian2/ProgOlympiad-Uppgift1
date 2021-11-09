ac0940a8bd5cc7ceb4cd7bb6c3e39642bd6c9fac
Caspian2 <89245401+Caspian2@users.noreply.github.com>
2021-11-09 15:05:53 +0100

    Add files via upload

---
+++ Uppgift 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv en mening : ");
+            string vanligaString = Console.ReadLine();
+            char[] stringArray = vanligaString.ToCharArray();
+            Array.Reverse(stringArray);
+            string omvändaString = new string(stringArray);
+            
+        
+            int wordCount = 0, index = 0;
+
+
+            while (index < vanligaString.Length && char.IsWhiteSpace(vanligaString[index]))
+            index++;
+
+            while (index < vanligaString.Length)
+            {
+  
+            while (index < vanligaString.Length && !char.IsWhiteSpace(vanligaString[index]))
+            index++;
+
+            wordCount++;
+
+
+            while (index < vanligaString.Length && char.IsWhiteSpace(vanligaString[index]))
+            index++;
+            }
+
+            Console.WriteLine($"Svar : {omvändaString} ");
+            Console.WriteLine("Antal ord " + wordCount);
+            Console.ReadLine();
+        }  
+    }
+}
